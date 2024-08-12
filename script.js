e87ea852099fb2e9eefb42cfe42b32bff186d7e4
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 14:18:46 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Script running...")
+// Task 1: 
+// Select the cshImage div
+
+
+// Add an event listener such that when the div is clicked, you create a new p tag, set the text to "Our new building!" and append it to the div
+
+
+
+
+
+// Task 2: 
+// Set up the DOM manipulation flow such that when you click the cat button, the cat image is removed.
+
+
+
+
+// Set up the DOM manipulation flow such that when you click the dog button, the dog image is removed.
+
+
+
+
+// Task 3: Set up the DOM manipulation flow such that when the add button is clicked, you create a new li tag, set the text to the value of input, and append it to the ul element
+
+
+
+
+
